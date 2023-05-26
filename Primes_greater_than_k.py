4907ed0868d286ab55cd275078d5dadf5650eeb8
Upanisha10 <120163856+Upanisha10@users.noreply.github.com>
2023-05-26 12:09:18 +0530

    def prime(n):     cnt = 0     for i in range(1,n+1):         if n%i==0:             cnt+=1     if cnt==2:         return 1     else:         return 0 n = int(input()) arr = list(map(int,input().split())) k = int(input()) cnt=0 for i in arr:     if prime(i) and i>=k:         cnt+=1 print(cnt)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,17 @@
+def prime(n):
+    cnt = 0
+    for i in range(1,n+1):
+        if n%i==0:
+            cnt+=1
+    if cnt==2:
+        return 1
+    else:
+        return 0
+n = int(input())
+arr = list(map(int,input().split()))
+k = int(input())
+cnt=0
+for i in arr:
+    if prime(i) and i>=k:
+        cnt+=1
+print(cnt)

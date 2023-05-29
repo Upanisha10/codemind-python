8c92507cb43a2695cacded5abe2a54b39bc46b8a
Upanisha10 <120163856+Upanisha10@users.noreply.github.com>
2023-05-29 10:33:38 +0530

    def dc(n):     cnt = 0     while n>0:         cnt+=1         n//=10     return cnt n = int(input()) sq = n**2 dig = sq%(10**dc(n)) if dig == n:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,12 +1,12 @@
 def dc(n):
     cnt = 0
     while n>0:
+        cnt+=1
         n//=10
-        cnt += 1
     return cnt
 n = int(input())
 sq = n**2
-dig = sq % (10**dc(n))
+dig = sq%(10**dc(n))
 if dig == n:
     print("Automorphic Number")
 else:
